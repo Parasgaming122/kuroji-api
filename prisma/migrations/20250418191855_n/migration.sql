90bd0f2048c9b1b7dd8adf959cb59fa5c174ee84
Daffine <168743697+daffineq@users.noreply.github.com>
2025-04-18 22:19:53 +0300

    refactor: shikimori adjusted

---
+++ prisma/migrations/20250418191855_n/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `posterId` on the `Shikimori` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Shikimori" DROP COLUMN "posterId";
