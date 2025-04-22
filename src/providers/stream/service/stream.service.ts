f05545d68da3ea6fd4840c7c312eb70ceaffeac2
Daffine <168743697+daffineq@users.noreply.github.com>
2025-04-22 11:27:40 +0300

    feat: improved stream service

--- src/providers/stream/service/stream.service.ts
+++ src/providers/stream/service/stream.service.ts
@@ -6,7 +6,7 @@ import { AnimekaiService } from '../../animekai/service/animekai.service'
 import { AnimepaheService } from '../../animepahe/service/animepahe.service'
 import { AnilistService } from '../../anilist/service/anilist.service'
 import { TmdbService } from '../../tmdb/service/tmdb.service'
-import { AnilistStreamingEpisode, AnimeKai, AnimekaiEpisode, Animepahe, AnimepaheEpisode, EpisodeZoro, TmdbSeasonEpisode, Zoro } from '@prisma/client'
+import { AnilistStreamingEpisode, AnimekaiEpisode, AnimepaheEpisode, EpisodeZoro, TmdbSeasonEpisode, Zoro } from '@prisma/client'
 import { Source } from '../model/Source'
 
 export interface Episode {
@@ -15,6 +15,7 @@ export interface Episode {
   number: number,
   overview: string,
   date: string,
+  filler: boolean,
   sub: boolean,
   dub: boolean
 }
@@ -38,54 +39,47 @@ export class StreamService {
 
   async getEpisodes(id: number): Promise<Episode[]> {
     try {
-      const [aniwatch, season] = await Promise.all([
+      const [aniwatch, season, anilist] = await Promise.all([
         this.aniwatch.getZoroByAnilist(id).catch(() => null),
-        this.tmdb.getTmdbSeasonByAnilist(id).catch(() => null)
-      ]);
+        this.tmdb.getTmdbSeasonByAnilist(id).catch(() => null),
+        this.anilist.getAnilist(id).catch(() => null),
+      ])
 
-      if (!season || !season.episodes) {
-        const anilist = await this.anilist.getAnilist(id);
-        const streamingEpisodes = anilist.streamingEpisodes as AnilistStreamingEpisode[];
-        const episodes: Episode[] = streamingEpisodes.map((episode: any) => {
-          const match = episode.title.match(/.*?(\d+).*/);
-          const episodeNumber = match ? parseInt(match[1], 10) : 0;
-          const sub = aniwatch ? this.isEpisodeAvailable(aniwatch, episodeNumber, true) : false;
-          const dub = aniwatch ? this.isEpisodeAvailable(aniwatch, episodeNumber, false) : false;
+      const episodesZoro = aniwatch?.episodes as EpisodeZoro[] || []
+      const tmdbEpisodes = season?.episodes as TmdbSeasonEpisode[] || []
+      const anilistEpisodes = anilist?.streamingEpisodes as AnilistStreamingEpisode[] || []
 
-          return {
-            title: episode.title,
-            image: episode.thumbnail,
-            number: episodeNumber,
-            overview: "",
-            date: "",
-            sub,
-            dub,
-          };
-        });
-        return episodes;
-      }
+      const episodes: Episode[] = episodesZoro.map((episode) => {
+        const {
+          isFiller: filler = false,
+          isSubbed: sub = false,
+          isDubbed: dub = false,
+          number,
+          title: zoroTitle,
+        } = episode
 
-      const seasonEpisodes = season.episodes as TmdbSeasonEpisode[];
+        const tmdbEpisode = tmdbEpisodes.find(e => e.episode_number === number)
 
-      const episodes: Episode[] = seasonEpisodes.map((episode: TmdbSeasonEpisode) => {
-        const episodeNumber = episode.episode_number || 0;
-        const sub = aniwatch ? this.isEpisodeAvailable(aniwatch, episodeNumber, true) : false;
-        const dub = aniwatch ? this.isEpisodeAvailable(aniwatch, episodeNumber, false) : false;
+        const anilistEpisode = anilistEpisodes.find(e => {
+          const match = e.title?.match(/.*?(\d+).*/)
+          return match ? parseInt(match[1]) === number : false
+        })
 
         return {
-          title: episode.name || "",
-          image: episode.still_path || "",
-          number: episodeNumber,
-          overview: episode.overview || "",
-          date: episode.air_date || "",
+          title: tmdbEpisode?.name || anilistEpisode?.title || zoroTitle,
+          image: tmdbEpisode?.still_path || anilistEpisode?.thumbnail || "",
+          number,
+          overview: tmdbEpisode?.overview ?? "",
+          date: tmdbEpisode?.air_date ?? "",
+          filler,
           sub,
           dub,
-        };
-      });
+        }
+      })
 
-      return episodes;
+      return episodes
     } catch (e) {
-      throw new Error(e);
+      throw new Error(String(e))
     }
   }
 
@@ -112,84 +106,43 @@ export class StreamService {
   }
 
   async getProvidersSingle(id: number, ep: number): Promise<ProviderInfo[]> {
-    const zoroProviders: ProviderInfo[] = []
-    const paheProviders: ProviderInfo[] = []
-    const kaiProviders: ProviderInfo[] = []
+    const providers: ProviderInfo[] = []
 
-    const zoroPromise = this.aniwatch.getZoroByAnilist(id)
-      .then((zoro: Zoro) => {
-        if (zoro && zoro.episodes) {
-          const zoroEps = zoro.episodes as EpisodeZoro[]
-          const zoroEp = zoroEps.find((e: EpisodeZoro) => e.number === ep)
-          if (zoroEp) {
-            if (zoroEp.isSubbed) {
-              zoroProviders.push({
-                id: zoroEp.id,
-                filler: zoroEp.isFiller,
-                provider: Provider.ANIWATCH,
-                type: SourceType.SOFT_SUB,
-              })
-            }
-            if (zoroEp.isDubbed) {
-              zoroProviders.push({
-                id: zoroEp.id,
-                filler: zoroEp.isFiller,
-                provider: Provider.ANIWATCH,
-                type: SourceType.DUB,
-              })
-            }
-          }
-        }
-      })
-      .catch(() => { })
+    const [zoro, pahe, kai] = await Promise.all([
+      this.aniwatch.getZoroByAnilist(id).catch(() => null),
+      this.animepahe.getAnimepaheByAnilist(id).catch(() => null),
+      this.animekai.getAnimekaiByAnilist(id).catch(() => null),
+    ])
 
-    const animepahePromise = this.animepahe.getAnimepaheByAnilist(id)
-      .then((animepahe: Animepahe) => {
-        if (animepahe && animepahe.episodes) {
-          const paheEps = animepahe.episodes as AnimepaheEpisode[]
-          const paheEp = paheEps.find((e: AnimepaheEpisode) => e.number === ep)
-          if (paheEp) {
-            paheProviders.push({
-              id: paheEp.id,
-              filler: false,
-              provider: Provider.ANIMEPAHE,
-              type: SourceType.BOTH,
-            })
-          }
-        }
-      })
-      .catch(() => { })
+    const pushProvider = (
+      id: string,
+      filler: boolean,
+      provider: Provider,
+      type: SourceType
+    ) => {
+      providers.push({ id, filler, provider, type })
+    }
 
-    const animeKaiPromise = this.animekai.getAnimekaiByAnilist(id)
-      .then((animeKai: AnimeKai) => {
-        if (animeKai && animeKai.episodes) {
-          const kaiEps = animeKai.episodes as AnimekaiEpisode[]
-          const kaiEp = kaiEps.find((e: AnimekaiEpisode) => e.number === ep)
-          if (kaiEp) {
-            if (kaiEp.isSubbed) {
-              kaiProviders.push({
-                id: kaiEp.id,
-                filler: kaiEp.isFiller || false,
-                provider: Provider.ANIMEKAI,
-                type: SourceType.HARD_SUB,
-              })
-            }
-            if (kaiEp.isDubbed) {
-              kaiProviders.push({
-                id: kaiEp.id,
-                filler: kaiEp.isFiller || false,
-                provider: Provider.ANIMEKAI,
-                type: SourceType.DUB,
-              })
-            }
-          }
-        }
-      })
-      .catch(() => { })
+    const zoroEp = (zoro?.episodes as EpisodeZoro[])?.find((e: EpisodeZoro) => e.number === ep)
+    if (zoroEp) {
+      const { id, isFiller, isSubbed, isDubbed } = zoroEp
+      if (isSubbed) pushProvider(id, isFiller, Provider.ANIWATCH, SourceType.SOFT_SUB)
+      if (isDubbed) pushProvider(id, isFiller, Provider.ANIWATCH, SourceType.DUB)
+    }
 
-    await Promise.all([zoroPromise, animepahePromise, animeKaiPromise])
+    const paheEp = (pahe?.episodes as AnimepaheEpisode[])?.find((e: AnimepaheEpisode) => e.number === ep)
+    if (paheEp) {
+      pushProvider(paheEp.id, false, Provider.ANIMEPAHE, SourceType.BOTH)
+    }
 
-    return [...zoroProviders, ...paheProviders, ...kaiProviders]
+    const kaiEp = (kai?.episodes as AnimekaiEpisode[])?.find((e: AnimekaiEpisode) => e.number === ep)
+    if (kaiEp) {
+      const { id, isFiller = false, isSubbed, isDubbed } = kaiEp
+      if (isSubbed) pushProvider(id, false, Provider.ANIMEKAI, SourceType.HARD_SUB)
+      if (isDubbed) pushProvider(id, false, Provider.ANIMEKAI, SourceType.DUB)
+    }
+
+    return providers
   }
 
   async getProvidersMultiple(id: number): Promise<(Episode & { providers: ProviderInfo[] })[]> {
@@ -210,44 +163,22 @@ export class StreamService {
 
   async getSources(provider: Provider, ep: number, alId: number, dub: boolean): Promise<Source> {
     const providers = await this.getProvidersSingle(alId, ep)
-    const epId = providers.find(p => p.provider === provider)?.id;
-    
-    switch (provider) {
-      case Provider.ANIWATCH: {
-        const zoro = await this.aniwatch.getZoroByAnilist(alId).catch(() => null);
-        if (zoro && zoro.episodes) {
-          const zoroEps = zoro.episodes as EpisodeZoro[];
-          const zoroEp = zoroEps.find(e => e.id === epId);
-          if (zoroEp) {
-            return await this.aniwatch.getSources(zoroEp.id, dub);
-          }
-        }
-        break;
-      }
-      case Provider.ANIMEKAI: {
-        const animeKai = await this.animekai.getAnimekaiByAnilist(alId).catch(() => null);
-        if (animeKai && animeKai.episodes) {
-          const kaiEps = animeKai.episodes as AnimekaiEpisode[];
-          const kaiEp = kaiEps.find(e => e.id === epId);
-          if (kaiEp) {
-            return await this.animekai.getSources(kaiEp.id, dub);
-          }
-        }
-        break;
-      }
-      case Provider.ANIMEPAHE: {
-        const animepahe = await this.animepahe.getAnimepaheByAnilist(alId).catch(() => null);
-        if (animepahe && animepahe.episodes) {
-          const paheEps = animepahe.episodes as AnimepaheEpisode[];
-          const paheEp = paheEps.find(e => e.id === epId);
-          if (paheEp) {
-            return await this.animepahe.getSources(paheEp.id);
-          }
-        }
-        break;
-      }
+    const epId = providers.find(p => p.provider === provider)?.id
+    if (!epId) throw new Error("Episode not found for provider")
+
+    const fetchMap = {
+      [Provider.ANIWATCH]: async () => this.aniwatch.getSources(epId, dub),
+      [Provider.ANIMEKAI]: async () => this.animekai.getSources(epId, dub),
+      [Provider.ANIMEPAHE]: async () => this.animepahe.getSources(epId),
     }
-    
-    return Promise.reject(Error('No sources found'));
+
+    const fetchFn = fetchMap[provider]
+    if (!fetchFn) throw new Error("Invalid provider")
+
+    try {
+      return await fetchFn()
+    } catch {
+      throw new Error("No sources found")
+    }
   }
 }
