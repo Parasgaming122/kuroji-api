57266381cdb3075f640858fff757ec0a86a20144
daffineq <daffineq@users.noreply.github.com>
2025-04-27 22:25:42 +0300

    chore: endpoints changed for tmdb and tvdb

--- src/providers/tmdb/controller/tmdb.controller.ts
+++ src/providers/tmdb/controller/tmdb.controller.ts
@@ -1,16 +1,16 @@
 import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { TmdbService } from '../service/tmdb.service';
 
-@Controller('tmdb')
+@Controller('anime')
 export class TmdbController {
   constructor(private readonly service: TmdbService) {}
 
-  @Get('info/:id')
+  @Get('info/:id/tmdb')
   async getTmdbByAnilist(@Param('id', ParseIntPipe) id: number) {
     return this.service.getTmdbByAnilist(id);
   }
 
-  @Get('info/:id/season')
+  @Get('info/:id/tmdb/season')
   async getTmdbSeasonByAnilist(@Param('id', ParseIntPipe) id: number) {
     return this.service.getTmdbSeasonByAnilist(id);
   }
--- src/providers/tvdb/controller/tvdb.controller.ts
+++ src/providers/tvdb/controller/tvdb.controller.ts
@@ -1,26 +1,26 @@
 import { Controller, Get, Param, ParseIntPipe, Put } from '@nestjs/common';
 import { TvdbService } from '../service/tvdb.service'
 
-@Controller('tvdb')
+@Controller('anime')
 export class TvdbController {
   constructor(private readonly service: TvdbService) {}
 
-  @Get('info/:id')
+  @Get('info/:id/tvdb')
   async getTvdbByAnilist(@Param('id', ParseIntPipe) id: number) {
     return this.service.getTvdbByAnilist(id);
   }
 
-  @Get('info/:id/translations/:language')
+  @Get('info/:id/tvdb/translations/:language')
   async getTvdbTranslation(@Param('id', ParseIntPipe) id: number, @Param('language') language: string) {
     return this.service.getTranslations(id, language);
   }
 
-  @Get('languages')
+  @Get('tvdb/languages')
   async getLanguages() {
     return this.service.getLanguages();
   }
 
-  @Put('languages')
+  @Put('tvdb/languages')
   async updateLanguages() {
     return this.service.updateLanguages()
   }
