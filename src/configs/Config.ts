3f651089a29095c1c0ed655be95e32459fcf77fd
daffine <168743697+daffineq@users.noreply.github.com>
2025-05-03 10:43:28 +0300

    chore: custom api keys

--- src/configs/Config.ts
+++ src/configs/Config.ts
@@ -1,8 +1,10 @@
 export default class Config {
   public static readonly BASE = process.env.PUBLIC_URL;
 
-  public static readonly UPDATE_ENABLED = (process.env.UPDATE_ENABLED ?? 'true') === 'true';
-  public static readonly ANILIST_INDEXER_UPDATE_ENABLED = (process.env.ANILIST_INDEXER_UPDATE_ENABLED ?? 'true') === 'true';
+  public static readonly UPDATE_ENABLED =
+    (process.env.UPDATE_ENABLED ?? 'true') === 'true';
+  public static readonly ANILIST_INDEXER_UPDATE_ENABLED =
+    (process.env.ANILIST_INDEXER_UPDATE_ENABLED ?? 'true') === 'true';
 
   public static readonly ANILIST = process.env.ANILIST;
   public static readonly SHIKIMORI = process.env.SHIKIMORI;
@@ -10,4 +12,7 @@ export default class Config {
   public static readonly TMDB = process.env.TMDB;
   public static readonly TVDB = process.env.TVDB;
   public static readonly JIKAN = process.env.JIKAN;
+
+  public static readonly TMDB_API = process.env.TMDB_API;
+  public static readonly TVDB_API = process.env.TVDB_API;
 }
